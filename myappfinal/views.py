f09558936d6984045f69498134b7b8dd5a0d890c
csit <pooja@csitpark.com>
2024-03-21 13:05:03 +0530

    Search Changes

--- myappfinal/views.py
+++ myappfinal/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render 
 from .models import Property
+from .models import Area
+from .models import Category
 # Create your views here.
 def about(request):
     # return HttpResponse('about')
@@ -13,18 +15,31 @@ def error(request):
     return render(request,template_name='error.html')
 def index(request):
     # return HttpResponse('about')
-    properties = Property.objects.filter(PropertyName__in =['showroom','House','shop','villa','plot','Gaming studio','Tree house']).values()
-    context = {'properties': properties}
-    return render(request,'index.html',context)
+    if request.method == "POST": 
+        print(request.POST)
+        searchCategory =request.POST.get('category')
+        searchArea=request.POST.get('area')
+        print(searchCategory)
+        print(searchArea)
+        propertysearch=Property.objects.filter(Category=searchCategory, Area=searchArea)
+        return render(request,'index.html',{"data":propertysearch})
+    else:
+        displayproperty=Property.objects.all()
+        print(displayproperty)
+        return render(request,'index.html',{"data":displayproperty})
+
 def propertyagent(request):
     # return HttpResponse('about')
     return render(request,template_name='propertyagent.html')
+
 def propertylist(request):
     # return HttpResponse('about')
     return render(request,template_name='propertylist.html')
+
 def propertytype(request):
     # return HttpResponse('about')
     return render(request,template_name='propertytype.html')
+
 def testimonial(request):
     # return HttpResponse('about')
     return render(request,template_name='testimonial.html')
